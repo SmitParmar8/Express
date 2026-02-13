04314684172acba82e10e56713d311999f676d67
Smit Parmar <smitparmar@Smits-MacBook-Air.local>
2026-02-13 15:03:27 +0530

    Add day 3 progress i new branch

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+// testing the api's
 app.get('/', (req, res) => {
     console.log("user request catched ")
-  res.send('Hello World!')
+  res.status(200).send('Hello World!')
+})
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+app.delete('/user', (req, res) => {
+  console.log("able to hit the route ")
+  res.end();
 })
 
 app.listen(port, () => {
